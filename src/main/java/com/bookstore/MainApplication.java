c143d7d929acd486212f4c802a923943a348354f
SridharChallabotla <sridhar.challabotla@metlife.com>
2020-01-21 18:10:39 +0530

    first commit

---
+++ src/main/java/com/bookstore/MainApplication.java
@@ -0,0 +1,49 @@
+package com.bookstore;
+
+import com.bookstore.impl.BatchRepositoryImpl;
+import com.bookstore.service.BookstoreService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = BatchRepositoryImpl.class)
+public class MainApplication {
+
+    private final BookstoreService bookstoreService;
+
+    public MainApplication(BookstoreService bookstoreService) {
+        this.bookstoreService = bookstoreService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+            bookstoreService.batchAuthors();
+        };
+    }
+}
+
+/*
+Batch Inserts In Spring Boot Style And Batch Per Transaction
+
+Description: Batch inserts (in MySQL) in Spring Boot style. This example commits the database transaction after each batch excecution. This way we avoid long-running transactions and, in case of a failure, we rollback only the failed batch and don't lose the previous batches.
+
+Key points:
+
+in application.properties set spring.jpa.properties.hibernate.jdbc.batch_size
+in application.properties set spring.jpa.properties.hibernate.generate_statistics (just to check that batching is working)
+in application.properties set JDBC URL with rewriteBatchedStatements=true (optimization for MySQL)
+in application.properties set JDBC URL with cachePrepStmts=true (enable caching and is useful if you decide to set prepStmtCacheSize, prepStmtCacheSqlLimit, etc as well; without this setting the cache is disabled)
+in application.properties set JDBC URL with useServerPrepStmts=true (this way you switch to server-side prepared statements (may lead to signnificant performance boost))
+in case of using a parent-child relationship with cascade persist (e.g. one-to-many, many-to-many) then consider to set up spring.jpa.properties.hibernate.order_inserts=true to optimize the batching by ordering inserts
+in entity, use the assigned generator since the Hibernate IDENTITY will cause insert batching to be disabled
+in your DAO layer, commit the database transaction after each batch execution
+if is not needed then ensure that Second Level Cache is disabled via spring.jpa.properties.hibernate.cache.use_second_level_cache=false
+*/
